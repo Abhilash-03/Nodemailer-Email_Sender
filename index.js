b7140607af0155a338fb41a89c2ef11b9568940d
Abhilash <abhilashjha335@gmail.com>
2023-11-25 22:26:50 +0530

    bug fixes

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import dotenv from 'dotenv';
-
+import path from 'path';
+import { fileURLToPath } from 'url';
 import messageRoute from './routes/message.js';
 
 dotenv.config();
@@ -8,10 +9,13 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5000;
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 app.use(express.json());
 app.use(express.urlencoded({extended: false}))
 
-app.use(express.static('public'));
+app.use(express.static(path.join(__dirname, "public")));
 
 app.get('/', (req, res) => {
     res.send("<h1>App is listening you....</h1>");
